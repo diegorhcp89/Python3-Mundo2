d330c76bd4f5bd9683cfced75b94466c6cd30941
Diego Ferreira do Amaral <diegoferreiradoamaral@gmail.com>
2022-10-31 16:21:10 -0300

    Adicionando exercicio 39

--- Condições em Python(if..elif)/exercicios/ex039 - Alistamento Militar.py
+++ Condições em Python(if..elif)/exercicios/ex039 - Alistamento Militar.py
@@ -2,7 +2,6 @@ from datetime import date
 atual = date.today().year
 nasc = int(input('Ano de nascimento: '))
 idade = atual - nasc
-print('Quem nasceu em {} tem {} anos em {}.'. format(nasc, idade, atual))
 if idade == 18:
     print('Você tem que se alistar IMEDIATAMENTE')
 elif idade < 18:
