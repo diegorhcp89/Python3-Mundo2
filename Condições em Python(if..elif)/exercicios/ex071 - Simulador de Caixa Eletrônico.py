29a64e8659a8474ef1fb359c4382dd7f1f9628af
Diego Ferreira do Amaral <diegoferreiradoamaral@gmail.com>
2022-11-16 10:43:18 -0300

    Adicionando exercicio 71 Simulador de caixa eletrônico

---
+++ Condições em Python(if..elif)/exercicios/ex071 - Simulador de Caixa Eletrônico.py
@@ -0,0 +1,27 @@
+print('=' * 30)
+print('{:^30}'.format('BANCO CEV'))
+print('=' * 30)
+valor = int(input('Que valor você quer sacar? R$ '))
+total = valor
+céd = 100
+totcéd = 0
+while True:
+    if total >= céd:
+        total -= céd
+        totcéd += 1
+    else:
+        if totcéd > 0:
+            print(f'Total de {totcéd} cédulas de R${céd}')
+        if céd == 100:
+            céd = 50
+        if céd == 50:
+            céd = 20
+        elif céd == 20:
+            céd = 10
+        elif céd == 10:
+            céd = 5
+        totcéd = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO CEV Tenha um bom dia!')
